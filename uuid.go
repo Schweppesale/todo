fe0a393bfcd1cd1498ea1a3ea04b3ace2bca66e1
John Tripi <Tripij1@gmail.com>
2016-11-05 00:08:08 -0400

    used a code linter

--- uuid.go
+++ uuid.go
@@ -1,6 +1,6 @@
 package todo
 
-// UUIDGenerator serves as a layer of abstraction around our UUID generator
+// UUIDGenerator serves as a layer of abstraction when creating a UUID
 type UUIDGenerator interface {
 	Generate() string
 }
