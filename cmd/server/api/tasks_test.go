ff49b4ab9bde468fa345ac39b2426b0d22a09dce
John Tripi <Tripij1@gmail.com>
2016-11-06 12:07:05 -0500

    added some tests

--- cmd/server/api/tasks_test.go
+++ cmd/server/api/tasks_test.go
@@ -1,1 +1,28 @@
 package api
+
+import (
+	"github.com/schweppesale/todo/tasks/memory/satori"
+	"github.com/schweppesale/todo/tasks/logger"
+	"github.com/schweppesale/todo/tasks/memory"
+	"testing"
+)
+
+func NewTestTaskService() TaskService {
+	uuidGenerator := satori.NewUUIDGenerator()
+	tasks := logger.NewTaskRepository(memory.NewTaskRepository(uuidGenerator))
+	return NewTaskService(tasks)
+}
+
+func TestTaskService_UpdateTask(t *testing.T) {
+	service := NewTestTaskService()
+	task, err := service.CreateTask("test", "description")
+	if(err != nil) {
+		t.Error(err)
+	}
+
+	uniqueID := task.UniqueID
+	task, err = service.UpdateTask(uniqueID, "test2", "description2")
+	if(err != nil) {
+		t.Error(err)
+	}
+}
